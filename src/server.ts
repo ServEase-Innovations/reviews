c53c7c123b9d9c978c9c675c58d7927991400ea0
Ronit <maity.ronit18@gmail.com>
2026-02-08 01:04:37 +0530

    some more fixes

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import app from "./app";
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || "3000", 10);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Reviews service running on port ${PORT}`);
 });
